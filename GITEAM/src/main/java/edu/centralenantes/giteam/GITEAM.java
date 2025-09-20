f2b4aab547c81c85229e0fab6a6bf78189805a23
Haytam <Haytam.bensribit@gmail.com>
2025-09-20 20:05:03 +0200

    TP2 OBJET

--- GITEAM/src/main/java/edu/centralenantes/giteam/GITEAM.java
+++ GITEAM/src/main/java/edu/centralenantes/giteam/GITEAM.java
@@ -11,6 +11,6 @@ package edu.centralenantes.giteam;
 public class GITEAM {
 
     public static void main(String[] args) {
-        System.out.println("Pull test");
+        System.out.println("Xtest");
     }
 }
